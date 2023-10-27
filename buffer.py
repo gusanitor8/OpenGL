e28ebfc03a555283685c1d9fa7df529387b877cd
gusanitor8 <gusanitor8@gmail.com>
2023-10-26 18:55:20 -0600

    resolved imports for other SOs than MacOS

--- buffer.py
+++ buffer.py
@@ -1,26 +1,5 @@
 import ctypes
-
-try:
-    import OpenGL as ogl
-
-    try:
-        import OpenGL.GL  # this fails in <=2020 versions of Python on OS X 11.x
-    except ImportError:
-        print('Drat, patching for Big Sur')
-        from ctypes import util
-
-        orig_util_find_library = util.find_library
-
-
-        def new_util_find_library(name):
-            res = orig_util_find_library(name)
-            if res: return res
-            return '/System/Library/Frameworks/' + name + '.framework/' + name
-
-
-        util.find_library = new_util_find_library
-except ImportError:
-    pass
+import OpenGL.GL as ogl
 from numpy import array, float32
 
 
@@ -29,27 +8,27 @@ class Buffer:
         self.vertBuffer = array(data, dtype=float32)
 
         # vertex buffer object
-        self.VBO = ogl.GL.glGenBuffers(1)
+        self.VBO = ogl.glGenBuffers(1)
 
         # vertex array object
-        self.VAO = ogl.GL.glGenVertexArrays(1)
+        self.VAO = ogl.glGenVertexArrays(1)
 
     def render(self):
-        ogl.GL.glBindBuffer(ogl.GL.GL_ARRAY_BUFFER, self.VBO)
-        ogl.GL.glBindVertexArray(self.VAO)
+        ogl.glBindBuffer(ogl.GL.GL_ARRAY_BUFFER, self.VBO)
+        ogl.glBindVertexArray(self.VAO)
 
         # especificar la informacion de los vertices
-        ogl.GL.glBufferData(ogl.GL.GL_ARRAY_BUFFER,  # buffer id
+        ogl.glBufferData(ogl.GL.GL_ARRAY_BUFFER,  # buffer id
                             self.vertBuffer.nbytes,  # tamano del buffer en bytes
                             self.vertBuffer,  # informacion a cargar en el buffer
-                            ogl.GL.GL_STATIC_DRAW)  # uso que se le va a dar al buffer
+                            ogl.GL_STATIC_DRAW)  # uso que se le va a dar al buffer
 
         ogl.GL.glVertexAttribPointer(0,  # attribute number
                                      3,  # size
-                                     ogl.GL.GL_FLOAT,  # type
-                                     ogl.GL.GL_FALSE,  # is normalized
+                                     ogl.GL_FLOAT,  # type
+                                     ogl.GL_FALSE,  # is normalized
                                      4 * 3,  # stride
                                      ctypes.c_void_p(0))  # array buffer offset
 
-        ogl.GL.glEnableVertexAttribArray(0)
-        ogl.GL.glDrawArrays(ogl.GL.GL_TRIANGLES, 0, int(len(self.vertBuffer) / 3))
+        ogl.glEnableVertexAttribArray(0)
+        ogl.glDrawArrays(ogl.GL_TRIANGLES, 0, int(len(self.vertBuffer) / 3))
--- gl.py
+++ gl.py
@@ -1,28 +1,7 @@
 import glm
-
-try:
-    import OpenGL as ogl
-
-    try:
-        import OpenGL.GL  # this fails in <=2020 versions of Python on OS X 11.x
-    except ImportError:
-        print('Drat, patching for Big Sur')
-        from ctypes import util
-
-        orig_util_find_library = util.find_library
-
-
-        def new_util_find_library(name):
-            res = orig_util_find_library(name)
-            if res: return res
-            return '/System/Library/Frameworks/' + name + '.framework/' + name
-
-
-        util.find_library = new_util_find_library
-except ImportError:
-    pass
-
+import OpenGL.GL as ogl
 from OpenGl.GL.shaders import compileProgram, compileShader
+
 
 class Renderer:
     def __init__(self, screen):
@@ -30,26 +9,26 @@ class Renderer:
         _, _, self.width, self.height = screen.get_rect()
         self.clearColor = [0, 0, 0]
 
-        ogl.GL.glEnable(ogl.GL.GL_DEPTH_TEST)
-        ogl.GL.glViewport(0, 0, self.width, self.height)
+        ogl.glEnable(ogl.GL_DEPTH_TEST)
+        ogl.glViewport(0, 0, self.width, self.height)
         self.scene = []
 
         self.active_shader = None
 
     def setShaders(self, vertex_shader, fragment_shader):
         if vertex_shader is not None and fragment_shader is not None:
-            self.active_shader = compileProgram(compileShader(vertex_shader, ogl.GL.GL_VERTEX_SHADER),
-                                                compileShader(fragment_shader, ogl.GL.GL_FRAGMENT_SHADER))
+            self.active_shader = compileProgram(compileShader(vertex_shader, ogl.GL_VERTEX_SHADER),
+                                                compileShader(fragment_shader, ogl.GL_FRAGMENT_SHADER))
 
         else:
             self.active_shader = None
 
     def render(self):
-        ogl.GL.glClearColor(self.clearColor[0], self.clearColor[1], self.clearColor[2], 1)
-        ogl.GL.glClear(ogl.GL.GL_COLOR_BUFFER_BIT | ogl.GL.GL_DEPTH_BUFFER_BIT)
+        ogl.glClearColor(self.clearColor[0], self.clearColor[1], self.clearColor[2], 1)
+        ogl.glClear(ogl.GL_COLOR_BUFFER_BIT | ogl.GL_DEPTH_BUFFER_BIT)
 
         if self.active_shader:
-            ogl.GL.glUseProgram(self.active_shader)
+            ogl.glUseProgram(self.active_shader)
 
         for obj in self.scene:
             obj.render()
