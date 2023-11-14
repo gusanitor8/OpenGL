8783d844ec638ad73e45212647bca95225512b63
GONZALEZ PINEDA <gon21438@uvg.edu.gt>
2023-11-13 20:14:14 -0600

    Primeros avances, esta version todavia no dibuja

--- gl.py
+++ gl.py
@@ -1,64 +1,17 @@
 import glm
 from OpenGL.GL import *
-from OpenGL.GL.shaders import compileProgram, compileShader
 
 
-class Renderer(object):
+class Renderer:
     def __init__(self, screen):
         self.screen = screen
         _, _, self.width, self.height = screen.get_rect()
+
         self.clearColor = [0, 0, 0]
 
         glEnable(GL_DEPTH_TEST)
         glViewport(0, 0, self.width, self.height)
 
-        self.scene = []
-        self.activeShader = None
-
-        # view matrix
-        self.camPosition = glm.vec3(0, 0, 0)
-        self.camRotation = glm.vec3(0, 0, 0)
-
-        # projection matrix
-        self.projectionMatrix = glm.perspective(glm.radians(60), self.width / self.height, 0.1, 1000.0)
-
-    def getViewMatrix(self):
-        identity = glm.mat4(1)
-
-        translateMat = glm.translate(identity, self.camPosition)
-
-        pitch = glm.rotate(identity, glm.radians(self.camRotation.x), glm.vec3(1, 0, 0))
-        yaw = glm.rotate(identity, glm.radians(self.camRotation.y), glm.vec3(0, 1, 0))
-        roll = glm.rotate(identity, glm.radians(self.camRotation.z), glm.vec3(0, 0, 1))
-
-        rotationMat = pitch * yaw * roll
-
-        camMatrix = translateMat * rotationMat
-
-        return glm.inverse(camMatrix)
-
-    def setShaders(self, vertex_shader, fragment_shader):
-        if vertex_shader is not None and fragment_shader is not None:
-            self.activeShader = compileProgram(compileShader(vertex_shader, GL_VERTEX_SHADER),
-                                               compileShader(fragment_shader, GL_FRAGMENT_SHADER))
-        else:
-            self.activeShader = None
-
     def render(self):
-        glClearColor(self.clearColor[0], self.clearColor[1], self.clearColor[2], 1)
+        glClearColor(self.clearColor[0], self.clearColor[1], self.clearColor[2], 1.0)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-        if self.activeShader is not None:
-            glUseProgram(self.activeShader)
-
-            glUniformMatrix4fv(glGetUniformLocation(self.activeShader, 'viewMatrix'),
-                               1, GL_FALSE,  glm.value_ptr(self.getViewMatrix()))
-
-            glUniformMatrix4fv(glGetUniformLocation(self.activeShader, 'projectionMatrix'),
-                               1, GL_FALSE, glm.value_ptr(self.projectionMatrix))
-
-        for obj in self.scene:
-            if self.activeShader is not None:
-                glUniformMatrix4fv(glGetUniformLocation(self.activeShader, 'projectionMatrix'),
-                                   1, GL_FALSE, glm.value_ptr(obj.getModelMatrix()))
-            obj.render()
--- openGL_2023.py
+++ openGL_2023.py
@@ -1,40 +1,18 @@
 import pygame
 from pygame.locals import *
-from model import Model
 from gl import Renderer
-from shaders import *
-import glm
-
-import sys
-print(sys.version)
 
 width = 960
 height = 540
 
 pygame.init()
 
-screen = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
+screen = pygame.display.set_mode((width,height), pygame.OPENGL | pygame.DOUBLEBUF)
 clock = pygame.time.Clock()
-
 rend = Renderer(screen)
-rend.setShaders(vertex_shader, fragment_shader)
-triangleData = [
-    -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-    0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-    0.0, 0.5, 0.0, 0.0, 0.0, 1.0
-]
-
-triangleModel = Model(triangleData)
-triangleModel.position.z = -5
-triangleModel.scale = glm.vec3(5, 5, 5)
-rend.scene.append(triangleModel)
 
 isRunning = True
-
 while isRunning:
-    deltaTime = clock.tick(60) / 1000
-    keys = pygame.key.get_pressed()
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isRunning = False
@@ -43,33 +21,9 @@ while isRunning:
             if event.key == pygame.K_ESCAPE:
                 isRunning = False
 
-    """
-    if keys[pygame.K_RIGHT]:
-        if rend.clearColor[0] < 1.0:
-            rend.clearColor[0] += deltaTime
 
-    elif keys[pygame.K_LEFT]:
-        if rend.clearColor[0] > 0.0:
-            rend.clearColor[0] -= deltaTime
 
-    elif keys[pygame.K_UP]:
-        if rend.clearColor[1] < 1.0:
-            rend.clearColor[1] += deltaTime
-
-    elif keys[pygame.K_DOWN]:
-        if rend.clearColor[1] > 0.0:
-            rend.clearColor[1] -= deltaTime
-
-    elif keys[pygame.K_x]:
-        if rend.clearColor[2] < 1.0:
-            rend.clearColor[2] += deltaTime
-
-    elif keys[pygame.K_z]:
-        if rend.clearColor[2] > 0.0:
-            rend.clearColor[2] -= deltaTime
-    """
-    rend.render()
-
+    deltaTime = clock.tick(60)/1000
     pygame.display.flip()
 
 pygame.quit()
